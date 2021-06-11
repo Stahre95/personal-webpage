67f52eaf4b1f0736428c6f5f549ff0839fed68ba
johanstahre <Johan@bk30.se>
2021-06-11 16:40:53 +0200

    added more fontawesome to package.json && made navigation menu more responsive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { actions } from '../../Features/activeView';
 import '../Navigation/Navigation.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
 
 function Navigation() {
     const [homeButtonIsClicked, setHomeButtonIsClicked] = useState(false);
     const [aboutButtonIsClicked, setAboutButtonIsClicked] = useState(false);
     const [portfolioButtonIsClicked, setPortfolioButtonIsClicked] = useState(false);
     const [contactButtonIsClicked, setContactButtonIsClicked] = useState(false);
+    const [menuClicked, setMenuClicked] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -38,10 +42,31 @@ function Navigation() {
         }, 250);
     }
 
+    const menuActive = () => {
+        if(menuClicked) {
+            return (
+                <FontAwesomeIcon icon={faWindowClose}/>
+            )
+        }else {
+            return (
+                <div className="navigation-menu-fabars">
+                    <FontAwesomeIcon icon={faBars} size="2x"/>
+                </div>
+            )
+        }
+    }
+
+    function handleClick () {
+        setMenuClicked(!menuClicked)
+    }
+
     return (
         <div className="navigation-container">
             <div className="navigation-menu">
                 <div className="navigation-name">Johan Stahre <span>portfolio</span></div>
+                <div className="navigation-menu-icon" onClick={handleClick}>
+                    {menuActive()}
+                </div>
                 <div className="navigation-menu-item" onClick={() => { setHomeButtonIsClicked(true); animationOnClick(dispatch, 'homeButton')}}>Home</div>
                 <div className="navigation-menu-item" onClick={() => { setAboutButtonIsClicked(true); animationOnClick(dispatch, 'aboutButton')}}>About</div>
                 <div className="navigation-menu-item" onClick={() => { setPortfolioButtonIsClicked(true); animationOnClick(dispatch, 'portfolioButton')}}>Portfoli</div>
