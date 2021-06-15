6315b424f54b27fecde1840b3b8d5f80227a6f09
johanstahre <Johan@bk30.se>
2021-06-15 11:37:05 +0200

    added icons and styling

--- src/components/AboutView/AboutPage.jsx
+++ src/components/AboutView/AboutPage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './AboutPage.css'
 import ProfileImage from '../../Images/profile_image.jpeg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHotel } from '@fortawesome/free-solid-svg-icons'
+import HtmlIcon  from '../Icons/HtmlIcon'
+import CssIcon from '../Icons/CssIcon'
+import StarIcon from '../Icons/StarIcon'
+import JsIcon from '../Icons/JsIcon'
+import ReactIcon from '../Icons/ReactIcon'
+// import KotlinIcon from '../Icons/KotlinIcon'
+import SwiftIcon from '../Icons/SwiftIcon'
 
 const AboutPage = () => {
 
-    function StarIcon (props) {
-        const {fill = 'none'} = props
-        return (
-            <svg className="w-6 h-6" fill={fill} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"></path>
-            </svg>
-        )
-    }
+    
+
+   
+
     return (
         <div className="about-container">
             {/* about me section  */}
@@ -28,63 +33,73 @@ const AboutPage = () => {
                  <table className="skill-table">
                      <tbody className="skill-table-body">
                          <tr>
-                             <td className="skill-language">HTML</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
+                             <td>
+                                 <div className="skill-icon"><HtmlIcon /></div>
+                                 <div className="skill-language">HTML5</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
+                             </td>
+                             <td>
+                                 <div className="skill-icon"><CssIcon /></div>
+                                 <div className="skill-language">CSS</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
+                             </td>
+                             <td>
+                             <div className="skill-icon"><JsIcon /></div>
+                                 <div className="skill-language">Javascript</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
                              </td>
                          </tr>
                          <tr>
-                             <td className="skill-language">CSS</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
+                             <td>
+                                 <div className="skill-icon"><ReactIcon /></div>
+                                 <div className="skill-language">React</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
                              </td>
-                         </tr>
-                         <tr>
-                             <td className="skill-language">Javascript</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
+                             <td>
+                                <div className="skill-icon"><SwiftIcon /></div>
+                                 <div className="skill-language">Swift</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
                              </td>
-                         </tr>
-                         <tr>
-                             <td className="skill-language">Reactjs</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                             </td>
-                         </tr>
-                         <tr>
-                             <td className="skill-language">Kotlin</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                             </td>
-                         </tr>
-                         <tr>
-                             <td className="skill-language">Swift</td>
-                             <td className="skill-rating">
-                                <StarIcon fill="yellow" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
-                                <StarIcon fill="white" />
+                             <td>
+                                 <div className="skill-icon"><FontAwesomeIcon icon={faHotel} size="4x" /></div>
+                                 <div className="skill-language">Kotlin</div>
+                                 <div className="skill-rating">
+                                     <StarIcon fill="yellow" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                     <StarIcon fill="white" />
+                                 </div>
                              </td>
                          </tr>
                      </tbody>
