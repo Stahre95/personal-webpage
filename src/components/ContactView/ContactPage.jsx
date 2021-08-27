4e1019ab73ebdbd857ed04ea292b907273029ead
johanstahre <Johan@bk30.se>
2021-08-27 13:28:16 +0200

    design changes

--- src/components/ContactView/ContactPage.jsx
+++ src/components/ContactView/ContactPage.jsx
@@ -1,9 +1,30 @@
 import { faEnvelope, faMapMarkerAlt, faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../ContactView/ContactPage.css'
 
 function ContactPage() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
+    const [feedback, setFeedback] = useState('')
+
+    const handleSubmit = () => {
+        if(name != '' && email != '' && subject != '' && message != '') {
+            console.log(
+                'name: ' + name,
+                'email: '+ email,
+                'subject ' + subject,
+                'message: ' + message
+            )
+            setFeedback('Thanks for your message! I will be in touch in a couple of days!')
+            
+        } else {
+            setFeedback('Please fill out all the fields correctly')
+        }
+        console.log('submit button clicked')
+    }
     return (
         <div className="contact-container">
             <div className="contact-content">
@@ -49,20 +70,21 @@ function ContactPage() {
                     <form action="#" className="contact-form">
                         <div className="fields">
                             <div className="field field-name">
-                                <input type="text" className="name-input" placeholder="Name" />
+                                <input type="text" className="name-input" placeholder="Name" onChange={(event) => setName(event.target.value)} />
                             </div>
                             <div className="field field-email">
-                                <input type="text" className="email-input" placeholder="Email" />
+                                <input type="text" className="email-input" placeholder="Email" onChange={(event) => setEmail(event.target.value)} />
                             </div>
                         </div>
                         <div className="field field-subject">
-                            <input type="text" className="subject-input" placeholder="Subject" />
+                            <input type="text" className="subject-input" placeholder="Subject" onChange={(event) => setSubject(event.target.value)} />
                         </div>
                         <div className="field field-textarea">
-                            <textarea cols="30" rows="10" className="field-message" placeholder="Your message..."></textarea>
+                            <textarea cols="30" rows="10" className="field-message" placeholder="Your message..." onChange={(event) => setMessage(event.target.value)}></textarea>
                         </div>
+                        <div className="err-message">{feedback}</div>
                         <div className="button-area">
-                            <button className="send-message" type="submit" name="send">Send message</button>
+                            <button className="send-message" type="submit" name="send" onClick={() => handleSubmit()}>Send message</button>
                         </div>
                     </form>
                 </div>
