e8962522736df8b7babb2caa3de1780ab1a5883b
stahre95 <johan@bk30.se>
2021-08-30 00:27:41 +0200

    Changed home design

--- src/components/HomeView/HomePage.jsx
+++ src/components/HomeView/HomePage.jsx
@@ -7,13 +7,18 @@ const HomePage = () => {
     const dispatch = useDispatch();
 
     return (
+        <div className="overlay">
         <div className="home-container">
             <div className="home-text-container">
-                <h1 className="text-1">Hello, my Name is...</h1>
-                <h1 className="text-2">Johan Stahre</h1>
-                <h1 className="text-3">and I'm a <span>Junior Developer</span></h1>
-                <button className="home-button" onClick={() => dispatch(actions.about())}>About me</button>
+                <h1 className="text-1">Hello, I am Johan Stahre</h1>
+                <h1 className="text-2">Application Developper</h1>
+                <h1 className="text-3">Based in Sweden</h1>
+                <p className="buttons"><span className="button-about-me" onClick={() => dispatch(actions.about())}>About me</span> / <span className="button-portfolio" onClick={() => dispatch(actions.portfolio())}>Portfolio</span></p>
+                {/*<button className="home-button" onClick={() => dispatch(actions.about())}>About me</button>
+                <button className="home-button" onClick={() => dispatch(actions.about())}>About me</button>*/}
             </div>
+            </div>
+           
         </div>
     )
 }
